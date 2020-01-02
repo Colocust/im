4c438e13e4b9a62a560eb961f0fd046104221bcc
colocust <516100080@qq.com>
2020-01-02 19:48:20 +0800

    修改Romm build方法

--- server/app/db/Room.php
+++ server/app/db/Room.php
@@ -9,6 +9,7 @@
 namespace db;
 
 
+use tiny\Logger;
 use tiny\MongoDB;
 
 class Room extends MongoDB {
@@ -24,7 +25,7 @@ class Room extends MongoDB {
   }
 
   public function buildByMembers(array $members): bool {
-    $value = self::where(self::members, '=', $members)->find();
+    $value = self::in(self::members, $members)->find();
     if (count($value) == 0) {
       return false;
     }
@@ -34,7 +35,8 @@ class Room extends MongoDB {
   }
 
   public function initByMembers(array $members): bool {
-    $_id = md5($members[0], $members[1]);
+    $_id = md5($members[0] . $members[1]);
+    Logger::getInstance()->info('roomId' . $_id);
     self::create([
       self::id => $_id,
       self::members => $members
